098bf01b5683c8b24f91fd6b7ac17c1869b5fcf3
hoje <814624827@qq.com>
2020-07-01 22:29:58 +0800

    ccc

--- cloud2020/cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud2020/cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -13,7 +13,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaMain7001 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaMain7001.class, args);
-        System.out.println("aa");
         System.out.println("cc");
     }
 
