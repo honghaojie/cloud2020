9e77706d559d063ad40caaa9d2aa59159a4809b4
hoje <814624827@qq.com>
2020-07-01 22:23:29 +0800

    bbb

--- cloud2020/cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud2020/cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -14,6 +14,7 @@ public class EurekaMain7001 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaMain7001.class, args);
         System.out.println("aa");
+        System.out.println("cc");
     }
 
 }
